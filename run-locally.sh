4f2a4dbc8df357b016446a930c96f9c8a8594c1a
Alistair Evans <alistair.evans@enclave.io>
2021-04-01 16:34:52 +0100

    Fix sh file

--- run-locally.sh
+++ run-locally.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t mkdocs-enclave .
-docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs mkdocs-enclave
